9594de79ff0657f6b158e6ed48f1148df220a8e8
MarioWindsor <mario@lazaro.in>
2019-12-04 12:32:41 +0530

    BugFix: Accidental Section Tag Deleted

--- pages/buy.php
+++ pages/buy.php
@@ -51,6 +51,7 @@ require_once __DIR__ . '/../inc/above.php';
 
 
 	<!-- investment section -->
+	<section class="investment-section local-modal">
 		<div class="container row">
 			<div class="column large-4 large-offset-1 medium-6 medium-offset-3 small-10 small-offset-1">
 				<div class="title h2">
