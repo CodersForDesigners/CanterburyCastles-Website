3dc2fc179c57c39e8a4db875f7a2db18e3a800cf
adi <adityabhat@lazaro.in>
2019-12-06 15:48:52 +0530

    Phone Traps : Add the phone traps (2)

--- inc/below.php
+++ inc/below.php
@@ -4,7 +4,7 @@
 			<!-- ~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/~/ -->
 
 			<!-- Contact section -->
-			<section class="contact-section overflow">
+			<section class="contact-section overflow qpid_login_site js_contact_form_section">
 				<div class="container row">
 
 					<div id="contact" class="contact-card fill-dark column large-6 large-offset-3 medium-8 medium-offset-2 small-12 text-center">
@@ -14,11 +14,11 @@
 						<div class="or"><span>OR</span></div>
 
 						<div class="contact-form">
-							<form class="js_contact_form_general ga_form">
+							<!-- Main form -->
+							<form class="js_contact_form ga_form" data-c="test-trac-3">
 								<div class="form-row space-min-bottom">
 									<label class="visuallyhidden">Name</label>
-									<input class="form-field input-large block js_name" type="text" placeholder="Name">
-								</div>
+									<input class="form-field input-large block" type="text" name="name" placeholder="Name" data-name="name">
 								</div>
 								<!-- <div class="form-row space-min-bottom">
 									<label class="visuallyhidden">Email</label>
@@ -26,12 +26,18 @@
 								</div> -->
 								<div class="form-row space-min-bottom">
 									<label class="visuallyhidden">Mobile</label>
-									<input class="form-field input-large block js_phone" type="tel" placeholder="Mobile">
+									<div class="block prefix-group" style="position: relative">
+										<select class="form-field input-large block js_phone_country_code" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; opacity: 0">
+											<?php include __DIR__ . '/../inc/phone-country-codes.php' ?>
+										</select>
+										<input class="prefix js_phone_country_code_label button" value="+91" style="pointer-events: none; width: 100%;">
+									</div>
+									<input class="form-field input-large block" type="tel" name="phone-number" placeholder="Mobile">
 								</div>
 								<div class="form-row space-min-bottom">
 									<label class="visuallyhidden">Budget</label>
-									<select class="form-field input-large block">
-										<option selected="" disabled="">Select Budget</option>
+									<select class="form-field input-large block" name="budget">
+										<option selected="" disabled="" value="" data-name="budget">Select Budget</option>
 										<option>Around Rs.30 Lakhs</option>
 										<option>Around Rs.60 Lakhs</option>
 										<option>Around Rs.90 Lakhs</option>
@@ -42,6 +48,28 @@
 									<input class="button button-large block" type="submit" value="Enquire Now">
 								</div>
 							</form>
+
+							<!-- OTP form -->
+							<form class="js_otp_form" style="display: none;">
+								<div class="form-row columns small-12 _medium-6">
+									<div class="otp-trap minimal">
+										<label class="block">
+											<span class="label inline text-neutral-1 text-uppercase">Enter the OTP</span>
+											<input class="otp block" type="text" name="otp">
+										</label>
+										<label class="submit block">
+											<span class="invisible label inline text-neutral-1 text-uppercase">Submit</span>
+											<button class="button block">→</button>
+										</label>
+									</div>
+								</div>
+								<br>
+								<div class="form-row columns small-12 large-6 clearfix hidden">
+									<div class="label strong text-neutral-1 opacity-50 text-uppercase inline-middle cursor-pointer float-left" tabindex="-1">Re-send OTP</div>
+									<div class="label strong text-neutral-1 opacity-50 text-uppercase inline-middle cursor-pointer float-right" tabindex="-1">Enter different number</div>
+								</div>
+							</form>
+
 						</div>
 
 					</div>
@@ -165,6 +193,12 @@
 
 	<script type="text/javascript" src="/js/modules/revealer.js"></script>
 	<script type="text/javascript" src="/plugins/slick/slick.js"></script>
+
+	<script type="text/javascript" src="/js/modules/cupid/utils.js"></script>
+	<script type="text/javascript" src="/js/modules/cupid/user.js"></script>
+
+	<script type="text/javascript" src="/js/forms.js"></script>
+	<script type="text/javascript" src="/js/login-prompts.js"></script>
 
 	<script type="text/javascript">
 
